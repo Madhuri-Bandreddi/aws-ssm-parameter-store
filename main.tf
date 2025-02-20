c3cfd8550f42621f2ef2fcd4cfaec7e67102bcd4
mammu <bandreddymammu599@gmail.com>
2025-02-20 17:50:11 +0530

    updating ./aws-ssm-parameter-store

---
+++ main.tf
@@ -0,0 +1,7 @@
+resource "aws_ssm_parameter" "parameters" {
+  count  = length(var.parameters)
+  name   = var.parameters[count.index].name
+  value  = var.parameters[count.index].value
+  type   = "String"
+  key_id = " 4be654e1-07dd-476e-9e60-d93ae9f7402b"
+}
