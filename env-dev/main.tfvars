1072342fa85ae13e122282f3022f552cc98019f2
mammu <bandreddymammu599@gmail.com>
2025-02-20 18:28:12 +0530

    terraform

--- env-dev/main.tfvars
+++ env-dev/main.tfvars
@@ -1,5 +1,5 @@
 parameters = [
-  { name = "dev.frontend.catalogue_url", value = "http://catalogue.madhari123.shop:8080/" },
+  { name = "dev.frontend.catalogue_url", value = "http://catalogue.madhari123.shop:8080/" }
   { name = "dev.frontend.cart_url", value = "http://cart.madhari123.shop:8080/" }
   { name = "dev.frontend.user_url", value = "http://user.madhari123.shop:8080/" }
   { name = "dev.frontend.payment_url", value = "http://payment.madhari123.shop:8080/" }
--- main.tf
+++ main.tf
@@ -1,13 +1,7 @@
-# resource "aws_ssm_parameter" "parameters" {
-#   count  = length(var.parameters)
-#   name   = var.parameters[count.index].name
-#   value  = var.parameters[count.index].value
-#   type   = "String"
-#   key_id = " 4be654e1-07dd-476e-9e60-d93ae9f7402b"
-# }
-
-resource "aws_ssm_parameter" "parameter" {
-  name  = "test.test1"
-  type  = "String"
-  value = "hello world"
+resource "aws_ssm_parameter" "parameters" {
+  count  = length(var.parameters)
+  name   = var.parameters[count.index].name
+  value  = var.parameters[count.index].value
+  type   = "String"
+  key_id = " 4be654e1-07dd-476e-9e60-d93ae9f7402b"
 }
