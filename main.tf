d6a45986920cca6447fc9aeed362f9ee04571687
mammu <bandreddymammu599@gmail.com>
2025-02-20 18:46:03 +0530

    terraform

--- main.tf
+++ main.tf
@@ -3,5 +3,5 @@ resource "aws_ssm_parameter" "parameters" {
   name   = var.parameters[count.index].name
   value  = var.parameters[count.index].value
   type   = "String"
-  key_id = " 4be654e1-07dd-476e-9e60-d93ae9f7402b"
+  key_id = "4be654e1-07dd-476e-9e60-d93ae9f7402b"
 }
